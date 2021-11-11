e9f00953d5f21f57c88c1d0d469a34b955fcccf1
rei1024 <satopen1729@gmail.com>
2021-11-11 22:57:00 +0900

    add example

--- src/apgl/ast/loop.ts
+++ src/apgl/ast/loop.ts
@@ -1,6 +1,7 @@
 import { APGLExpr } from "./core.ts";
 
 export class LoopAPGLExpr extends APGLExpr {
+    private kind: string = "loop";
     constructor(
         public readonly body: APGLExpr,
     ) {
