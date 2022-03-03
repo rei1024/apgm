3e1a43928b1e688f7b9505a82d028fc420ad5f4f
rei1024 <satopen1729@gmail.com>
2022-03-03 16:10:17 +0900

    move deps

--- src/apgl/optimizer/mod.ts
+++ src/apgl/optimizer/mod.ts
@@ -1,11 +1,10 @@
 import { ActionAPGLExpr, APGLExpr, SeqAPGLExpr } from "../ast/mod.ts";
-
 import {
     Action,
     HaltOutAction,
     NopAction,
     parseAction,
-} from "https://rei1024.github.io/proj/apgsembly-emulator-2/src/exports.js?2022_03_03_";
+} from "../../deps.ts";
 
 /**
  * 最適化
