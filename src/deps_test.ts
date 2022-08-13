778f822ed527a3389f3780248510fd33ec1466ae
rei1024 <satopen1729@gmail.com>
2022-08-14 00:55:57 +0900

    improve example programs

--- src/deps_test.ts
+++ src/deps_test.ts
@@ -3,7 +3,7 @@ export {
     assertThrows,
 } from "https://deno.land/std@0.152.0/testing/asserts.ts";
 
-export { runAPGsembly } from "https://rei1024.github.io/proj/apgsembly-emulator-2/src/exports.js?2022_08_07";
+export { runAPGsembly } from "./deps.ts";
 
 export function test(name: string, fn: () => void) {
     return Deno.test(name, fn);
