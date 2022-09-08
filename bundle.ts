927a9b92fca917c187e539c6440fb0cb31dd6c80
rei1024 <satopen1729@gmail.com>
2022-09-09 01:48:41 +0900

    minify

--- bundle.ts
+++ bundle.ts
@@ -1,5 +1,28 @@
 import { bundle } from "https://deno.land/x/emit@0.8.0/mod.ts";
 
+import * as swc from "https://deno.land/x/swc@0.2.1/mod.ts";
+
 const result = await bundle("./src/integration/mod.ts");
+
 const { code } = result;
-await Deno.writeTextFile("./dist/integration.js", code);
+
+const { code: minify } = swc.print(
+    swc.parse(
+        code,
+        { syntax: "ecmascript", script: false },
+    ),
+    { minify: true },
+);
+
+const outputPath = "./dist/integration.js";
+
+if (Deno.args.includes('check')) {
+    const current = await Deno.readTextFile(outputPath);
+    if (current.trim() !== minify.trim()) {
+        console.error('Run `deno task bundle`.');
+        Deno.exit(1);
+    }
+} else {
+    await Deno.writeTextFile(outputPath, minify);
+}
+
--- define-require.js
+++ define-require.js
@@ -1,6 +1,7 @@
 // global
 var require = {
     paths: {
-        'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs'
-    }
+        "vs":
+            "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs",
+    },
 };
