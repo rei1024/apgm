6ed3e4d19ae58606486e495054f79a6136a5c0b9
rei1024 <satopen1729@gmail.com>
2022-09-09 13:36:07 +0900

    update deps

--- bundle.ts
+++ bundle.ts
@@ -1,4 +1,4 @@
-import { bundle } from "https://deno.land/x/emit@0.8.0/mod.ts";
+import { bundle } from "https://deno.land/x/emit@0.9.0/mod.ts";
 
 import * as swc from "https://deno.land/x/swc@0.2.1/mod.ts";
 
@@ -9,7 +9,7 @@ const { code } = result;
 const { code: minify } = swc.print(
     swc.parse(
         code,
-        { syntax: "ecmascript", script: false },
+        { syntax: "ecmascript", script: false, importAssertions: true },
     ),
     { minify: true },
 );
@@ -19,10 +19,9 @@ const outputPath = "./dist/integration.js";
 if (Deno.args.includes('check')) {
     const current = await Deno.readTextFile(outputPath);
     if (current.trim() !== minify.trim()) {
-        console.error('Run `deno task bundle`.');
+        console.error('%cRun `deno task bundle`.', "color: red");
         Deno.exit(1);
     }
 } else {
     await Deno.writeTextFile(outputPath, minify);
 }
-
