1762a4dbd123d36e6202387f2cc92b584ded52f1
rei1024 <satopen1729@gmail.com>
2022-09-11 23:32:57 +0900

    update deps

--- bundle.ts
+++ bundle.ts
@@ -2,26 +2,29 @@ import { bundle } from "https://deno.land/x/emit@0.9.0/mod.ts";
 
 import * as swc from "https://deno.land/x/swc@0.2.1/mod.ts";
 
-const result = await bundle("./src/integration/mod.ts");
+async function bundleOrCheck(bundleEntryPath: string, outputPath: string, check: boolean) {
 
-const { code } = result;
+    const result = await bundle(bundleEntryPath);
 
-const { code: minify } = swc.print(
-    swc.parse(
-        code,
-        { syntax: "ecmascript", script: false, importAssertions: true },
-    ),
-    { minify: true },
-);
+    const { code } = result;
 
-const outputPath = "./dist/integration.js";
+    const { code: minify } = swc.print(
+        swc.parse(
+            code,
+            { syntax: "ecmascript", script: false, importAssertions: true },
+        ),
+        { minify: true },
+    );
 
-if (Deno.args.includes("check")) {
-    const current = await Deno.readTextFile(outputPath);
-    if (current.trim() !== minify.trim()) {
-        console.error("%cRun `deno task bundle`.", "color: red");
-        Deno.exit(1);
+    if (check) {
+        const current = await Deno.readTextFile(outputPath);
+        if (current.trim() !== minify.trim()) {
+            console.error("%cRun `deno task bundle`.", "color: red");
+            Deno.exit(1);
+        }
+    } else {
+        await Deno.writeTextFile(outputPath, minify);
     }
-} else {
-    await Deno.writeTextFile(outputPath, minify);
 }
+
+await bundleOrCheck("./src/integration/mod.ts", "./dist/integration.js", Deno.args.includes("check"));
