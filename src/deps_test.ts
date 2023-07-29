094e2c006d36c5b85db56e48ed924fe76f30dab5
rei1024 <satopen1729@gmail.com>
2023-07-29 23:07:12 +0900

    chore: update deps

--- src/deps_test.ts
+++ src/deps_test.ts
@@ -1,7 +1,7 @@
 export {
     assertEquals,
     assertThrows,
-} from "https://deno.land/std@0.195.0/assert/mod.ts";
+} from "https://deno.land/std@0.196.0/assert/mod.ts";
 
 export { runAPGsembly } from "./deps.ts";
 
