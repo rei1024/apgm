01af8b6482dcbc35453439d6616008ef7e119dad
rei1024 <satopen1729@gmail.com>
2023-09-05 00:03:00 +0900

    fix tsconfig.json

--- src/deps_test.ts
+++ src/deps_test.ts
@@ -1,7 +1,7 @@
 export {
     assertEquals,
     assertThrows,
-} from "https://deno.land/std@0.198.0/assert/mod.ts";
+} from "https://deno.land/std@0.200.0/assert/mod.ts";
 
 export { runAPGsembly } from "./deps.ts";
 
