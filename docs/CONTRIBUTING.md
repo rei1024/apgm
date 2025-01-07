8fa07b14eb0517695e77807fecfa95fb67417b9b
rei1024 <satopen1729@gmail.com>
2025-01-08 01:01:48 +0900

    feat: add l-system

--- docs/CONTRIBUTING.md
+++ docs/CONTRIBUTING.md
@@ -2,6 +2,8 @@
 
 Thank you for considering contributing!
 
-Please [create an issue](https://github.com/rei1024/apgm/issues/new) to report a bug or make a request. PRs are greatly appreciated.
+Please [create an issue](https://github.com/rei1024/apgm/issues/new) to report a
+bug or make a request. PRs are greatly appreciated.
 
-Please note that APGM maintains backward compatibility when making changes to ensure that code already written works.
+Please note that APGM maintains backward compatibility when making changes to
+ensure that code already written works.
---
+++ docs/l-system/l-system.ts
@@ -0,0 +1,100 @@
+// F + - X Y
+// 1 2 3 4 5
+
+// 1 1111
+
+// 1
+
+// nwse
+// 1234
+
+interface LSystem<T> {
+    axiom: Map<T, T[]>;
+    initial: T[];
+}
+
+/**
+ * Lindenmayer system
+ */
+export function* lSystem<T>(
+    { axiom, initial }: LSystem<T>,
+): Generator<T[], void, unknown> {
+    let current = initial;
+    while (true) {
+        yield current;
+        current = current.flatMap((c) => axiom.get(c) ?? [c]);
+    }
+}
+
+export function lSystemArray<T>({ axiom, initial }: LSystem<T>, n: number): T[][] {
+    if (!Number.isInteger(n)) {
+        throw Error("error");
+    }
+    const array: T[][] = [];
+    let i = 0;
+    for (const c of lSystem({ axiom, initial })) {
+        array.push(c);
+        i++;
+        if (n <= i) {
+            break;
+        }
+    }
+
+    return array;
+}
+
+export function dragonAt(n: number): string[] {
+    return lSystemArray(dragon, n)[n - 1] ?? [];
+}
+
+export function analysis(n: number) {
+    const array = dragonAt(n);
+    const res = array.filter((y) => y !== "X" && y !== "Y").join("").split("F")
+        .map((c) => {
+            if (c === "++-") {
+                return "+";
+            } else if (c === "+--") {
+                return "-";
+            }
+            return c;
+        });
+    // console.log(res);
+
+    return res;
+}
+
+export function* lSystemN<T>(
+    { axiom, initial }: LSystem<T>,
+    n: number,
+    i: number = 0,
+) {
+    for (const x of initial) {
+        if (n <= i) {
+            yield x;
+        } else {
+            const d = axiom.get(x);
+            if (d === undefined) {
+                yield x;
+            } else {
+                lSystemN({ axiom, initial: d }, n, i + 1);
+            }
+        }
+    }
+}
+
+export const dragon: LSystem<string> = {
+    axiom: new Map([
+        ["X", "X+YF+".split("")],
+        ["Y", "-FX-Y".split("")],
+    ]),
+    initial: [..."FX"],
+};
+
+// FX
+// [F]X
+// F[X]
+// F[X] X+[Y]F+
+// F[X] +F+
+
+// X -> +F+
+// Y -> -F-
